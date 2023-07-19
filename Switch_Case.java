a823b0ec0005028d96b50fa43a904e24b08407b8
Zaidshaikh0201 <139450283+Zaidshaikh0201@users.noreply.github.com>
2023-07-19 15:14:04 +0530

    Update Switch_Case.java

--- Switch_Case.java
+++ Switch_Case.java
@@ -87,3 +87,32 @@ public class Switch_Case {
 }
 	
 }
+
+/*
+package Zaid;
+
+import java.util.*;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int num1;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number-> ");
+		num1 = sc.nextInt();
+		
+		if (num1 % 2 == 0)
+		{
+			System.out.print("The Number is positive");
+		}
+		else
+		{
+			System.out.print("The Number is negative");
+		}
+		
+		
+		
+		
+	}
+}*/
+
+
