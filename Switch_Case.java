30def8a08e768f4efe5b8f69eff6a1d3803e601f
Zaidshaikh0201 <139450283+Zaidshaikh0201@users.noreply.github.com>
2023-07-19 15:15:38 +0530

    Update Switch_Case.java

--- Switch_Case.java
+++ Switch_Case.java
@@ -116,3 +116,37 @@ public class EvenOdd {
 }*/
 
 
+/*
+package Zaid;
+
+import java.util.*;
+
+public class Third_ka_second {
+	public static void main(String[] args) {
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first number-> ");
+		num1 = sc.nextInt();
+		System.out.print("Enter the second number-> ");
+		num2 = sc.nextInt();
+		System.out.print("Enter the third number-> ");
+		num3 = sc.nextInt();
+		
+		if (num1>num2 && num1>num3)
+		{
+			System.out.println(num1 +" is the greatest");
+		}
+		if (num2>num1 && num2>num3)
+		{
+			System.out.println(num2 +" is the greatest");
+		}
+		if (num3>num2 && num3>num1)
+		{
+			System.out.println(num3 +" is the greatest");
+		}
+	}
+}
+
+*/ 
+
+
